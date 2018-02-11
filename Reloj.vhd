af90717483377a7e5c8168bfca5c0721966fa2fa
AlmuHS <liberamenso10000@gmail.com>
2018-02-11 23:18:22 +0100

    replanteada puesta en hora

--- Reloj.vhd
+++ Reloj.vhd
@@ -47,12 +47,14 @@ port (clk1 : in std_logic;
       hours : out std_logic_vector(3 downto 0);
 		set_hour: in std_logic;
 		set_minutes: in std_logic;
-		set_t: in std_logic
+		set_t: in std_logic;
+		set_enable: out std_logic
      );
 end digi_clk;
 
-architecture Behavioral of digi_clk is	
-	signal sec,min,hour : integer range 0 to 60 :=0;
+architecture Behavioral of digi_clk is
+	signal min_set, sec_clk, min_clk: integer range 0 to 60 := 0;
+	signal hour_set, hour_clk: integer range 0 to 12 := 0; 
 	signal count : integer := 1;
 	signal clk : std_logic :='0';
 	
@@ -62,11 +64,47 @@ architecture Behavioral of digi_clk is
 	constant wait_toset: mystates := "10";
 	constant wait_toclock: mystates := "11";
 	
-	signal state: mystates;
+	signal state: mystates := clock;
+	
 begin
-	seconds <= conv_std_logic_vector(sec,6);
-	minutes <= conv_std_logic_vector(min,6);
-	hours <= conv_std_logic_vector(hour,4);
+	seconds <= conv_std_logic_vector(sec_clk,6) when(state = clock) else conv_std_logic_vector(0,6);
+	minutes <= conv_std_logic_vector(min_clk,6) when(state = clock) else  conv_std_logic_vector(min_set, 6);
+	hours <= conv_std_logic_vector(hour_clk,4) when(state = clock) else conv_std_logic_vector(hour_set,4);
+	set_enable <= set_t;
+
+	transitions: process(clk1)
+		begin
+			if(clk1'event and clk1='1') then
+				case state is	
+					when clock => if(set_t = '1') then state <= wait_toset; else state <= clock; end if;
+					when wait_toset => if(set_t = '0') then state <= set_time; else state <= wait_toset; end if;
+					when set_time => if(set_t = '1') then state <= wait_toclock; else state <= set_time; end if;
+					when wait_toclock => if(set_t = '0') then state <= clock; else state <= wait_toclock; end if;
+					when others => state <= clock;
+				end case;
+		end if;
+	end process;
+
+	process(set_t)
+	begin
+		if(state = set_time) then
+	
+			if(set_hour='1') then
+				if(hour_set = 12) then
+					hour_set <= 0;
+				else hour_set <= hour_set + 1;
+				end if;
+			end if;
+			
+			if(set_minutes='1') then
+				if(min_set < 59) then
+					min_set <= min_set + 1;
+				else min_set <= 0;
+				end if;
+			end if;
+		end if;
+		
+	end process;
 
 	 --clk generation.For 32 MHz clock this generates 1 Hz clock.
 	process(clk1)
@@ -80,50 +118,24 @@ begin
 		end if;
 	end process;
 
-
-	transitions: process(clk)
-		begin
-			if(clk'event and clk='1') then
-				case state is	
-					when clock => if(set_t = '1') then state <= wait_toset; end if;
-					when wait_toset => if(set_t = '0') then state <= set_time; end if;
-					when set_time => if(set_t = '1') then state <= wait_toclock; end if;
-					when wait_toclock => if(set_t = '0') then state <= clock; end if;
-					when others =>
-				end case;
-			end if;
-	end process;
-			
-	process(clk, state, set_hour, hour, set_minutes, min)   --period of clk is 1 second.
+	process(clk)   --period of clk is 1 second.
 	begin
-			if(clk'event and clk='1' and state = clock) then
-				sec <= sec + 1;
-				if(sec = 59) then
-					sec<=0;
-					min <= min + 1;
-					if(min = 59) then
-						hour <= hour + 1;
-						min <= 0;
-						if(hour = 12) then
-							hour <= 0;
-						end if;
+
+		if(clk'event and clk='1') then
+			sec_clk <= sec_clk + 1;
+			if(sec_clk = 59) then
+				sec_clk <=0;
+				min_clk <= min_clk + 1;
+				if(min_clk = 59) then
+					hour_clk <= hour_clk + 1;
+					min_clk <= 0;
+					if(hour_clk = 12) then
+						hour_clk <= 0;
 					end if;
 				end if;
 			end if;
-		
-				
-			if(state = set_time) then
-				if(set_hour = '1') then
-					if(hour < 11) then hour <= hour + 1;
-						else hour <= 0;
-					end if;
-					
-				elsif(set_minutes = '1') then
-					if(min < 59) then min <= min + 1;
-						else min <= 0;
-					end if;
-				end if;
-			end if;
+		end if;
+
 end process;
 
 end Behavioral;
