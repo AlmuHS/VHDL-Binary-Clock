9d84be4b599f66df0faea98ed2b4d3bc2ce62992
Almudena Garcia <liberamenso10000@gmail.com>
2018-11-20 16:11:36 +0100

    Revert "Update Reloj.vhd"

--- Reloj.vhd
+++ Reloj.vhd
@@ -59,48 +59,55 @@ architecture Behavioral of digi_clk is
 	signal count : integer := 1;
 	signal clk : std_logic :='0';
 	
-	constant clock: std_logic := '0';
-	constant set_time: std_logic := '1';
+	subtype mystates is std_logic_vector(1 downto 0);
+	constant clock: mystates := "00";
+	constant set_time: mystates := "01";
+	constant wait_toset: mystates := "10";
+	constant wait_toclock: mystates := "11";
 	
-	signal state: std_logic := clock;
+	signal state: mystates := clock;
 	
 begin
-	
-
-	
 	seconds <= conv_std_logic_vector(sec_clk,6) when(state = clock) else conv_std_logic_vector(0,6);
-	minutes <= conv_std_logic_vector(min_clk,6) when(state = clock and min_clk > min_set) else  conv_std_logic_vector(min_set, 6);
-	hours <= conv_std_logic_vector(hour_clk,4) when(state = clock and hour_clk > hour_set) else conv_std_logic_vector(hour_set,4);
-	set_enable <= state;
+	minutes <= conv_std_logic_vector(min_clk,6) when(state = clock) else  conv_std_logic_vector(min_set, 6);
+	hours <= conv_std_logic_vector(hour_clk,4) when(state = clock) else conv_std_logic_vector(hour_set,4);
+	set_enable <= set_t;
 
-	transitions: process(set_t)
-	begin
-		if(set_t = '1') then
-			case state is
-				when clock => state <= set_time;
-				when set_time => state <= clock;
-				when others => state <= clock;
-			end case;
+	transitions: process(clk, set_t)
+		begin
+			if(clk'event and clk='1') then
+				case state is	
+					when clock => if(set_t = '1') then state <= wait_toset; else state <= clock; end if;
+					when wait_toset => if(set_t = '0') then state <= set_time; else state <= wait_toset; end if;
+					when set_time => if(set_t = '1') then state <= wait_toclock; else state <= set_time; end if;
+					when wait_toclock => if(set_t = '0') then state <= clock; else state <= wait_toclock; end if;
+					when others => state <= clock;
+				end case;
 		end if;
 	end process;
 
-	process(set_hour, set_minutes, state)
+	process(set_hour, set_minutes, state, hour_clk, min_clk, hour_set, min_set)
 	begin
 		if(state = set_time) then
-			if(set_hour = '1') then
-				if(hour_set = 11) then
+	
+			if(set_hour='1') then
+				if(hour_set = 12) then
 					hour_set <= 0;
-				else 
-					hour_set <= hour_set + 1;
+				else hour_set <= hour_set + 1;
 				end if;
+			else hour_set <= hour_clk;
 			end if;
-		
+			
 			if(set_minutes='1') then
 				if(min_set < 59) then
 					min_set <= min_set + 1;
 				else min_set <= 0;
 				end if;
+			else min_set <= min_clk;
 			end if;
+		else
+			hour_set <= hour_clk;
+			min_set <= min_clk;
 		end if;
 	end process;
 
@@ -118,25 +125,24 @@ begin
 
 	process(clk)   --period of clk is 1 second.
 	begin
-		if(clk'event and clk='1') then
+		if(clk'event and clk='1' and state = clock) then
+ 			hour_clk <= conv_integer(hours);
 			min_clk <= conv_integer(minutes);
-			hour_clk <= conv_integer(hours);
 		
-			if(sec_clk < 59) then 
-				sec_clk <= sec_clk + 1;
+			if(sec_clk < 59) then sec_clk <= sec_clk + 1;
 			else 
 				sec_clk <= 0;
 				
 				if(min_clk < 59) then min_clk <= min_clk + 1;
 				else 
 					min_clk <= 0;
-					if(hour_clk < 12) then 
-						hour_clk <= hour_clk + 1;
+					if(hour_clk < 12) then hour_clk <= hour_clk + 1;
 					else hour_clk <= 0;
 					end if;
 				end if;
 			end if;
 		end if;
+
 end process;
 
 end Behavioral;
