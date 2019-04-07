bdfce019bd8fa9f01d15858a5f0e994ccadb05f6
almu <almu@debian>
2019-04-07 02:24:56 +0200

    try to improve logic

--- Reloj.vhd
+++ Reloj.vhd
@@ -58,81 +58,79 @@ architecture Behavioral of digi_clk is
 	signal hour_set, hour_clk: integer range 0 to 12 := 0; 
 	signal count : integer := 1;
 	
-	constant clock: std_logic := '0';
-	constant set_time: std_logic := '1';
+	constant clock: std_logic_vector(1 downto 0) := "00";
+	constant set_time: std_logic_vector(1 downto 0) := "01";
+	constant wait_to_clock: std_logic_vector(1 downto 0) := "10";
+	constant wait_to_set: std_logic_vector(1 downto 0) := "11";
 	
-	signal state: std_logic := clock;
+	signal state: std_logic_vector(1 downto 0) := clock;
 	
 begin
 		
 	seconds <= conv_std_logic_vector(sec_clk,6) when(state = clock) else conv_std_logic_vector(0,6);
 	minutes <= conv_std_logic_vector(min_clk,6) when(state = clock and min_clk > min_set) else  conv_std_logic_vector(min_set, 6);
 	hours <= conv_std_logic_vector(hour_clk,4) when(state = clock and hour_clk > hour_set) else conv_std_logic_vector(hour_set,4);
-	set_enable <= set_t;
+
+	set_enable <= '0' when(state = clock or state = wait_to_set) else '1';
 
 	transitions: process(set_t, clk)
 	begin
-		if(clk'event and clk='1' and set_t='1') then
+		if(clk'event and clk='1') then
 			case state is
-				when clock => state <= set_time;
-				when set_time => state <= clock;
+				when clock => if(set_t = '1') then state <= wait_to_set; else state <= clock; end if; 
+				when wait_to_set => if(set_t = '0') then state <= set_time; else state <= wait_to_set; end if;
+				when set_time => if(set_t = '1') then state <= wait_to_clock; else state <= set_time; end if;
+				when wait_to_clock => if(set_t = '0') then state <= clock; else state <= wait_to_clock; end if;
 				when others => state <= clock;
 			end case;
 		end if;
 	end process;
 	
-	process(set_hour, set_minutes, state, clk)
-	begin
-		if(clk'event and clk='1' and state = set_time) then
-			
-			if(set_hour = '1') then
-				if(hour_set = 11) then
-					hour_set <= 0;
-				else 
-					hour_set <= hour_set + 1;
-				end if;
-			end if;
-		
-			if(set_minutes='1') then
-				if(min_set < 59) then
-					min_set <= min_set + 1;
-				else min_set <= 0;
-				end if;
-			end if;
-			
-		end if;
-	end process;
-
 
 	process(clk)   --period of clk is 1 second.
 	begin
 		if(clk'event and clk='1') then
-			count <= count + 1;
-			
 			if(count = 16000000) then
-
 				count <= 0;
-				min_clk <= conv_integer(minutes);
-				hour_clk <= conv_integer(hours);
-			
-				if(sec_clk < 59) then 
-					sec_clk <= sec_clk + 1;
-				else 
-					sec_clk <= 0;
+
+				if(state = clock) then
+						min_clk <= conv_integer(minutes);
+						hour_clk <= conv_integer(hours);
 					
-					if(min_clk < 59) then min_clk <= min_clk + 1;
-					else 
-						min_clk <= 0;
-						if(hour_clk < 12) then 
-							hour_clk <= hour_clk + 1;
-						else hour_clk <= 0;
+						if(sec_clk < 59) then 
+							sec_clk <= sec_clk + 1;
+						else 
+							sec_clk <= 0;
+							
+							if(min_clk < 59) then min_clk <= min_clk + 1;
+							else 
+								min_clk <= 0;
+								if(hour_clk < 12) then 
+									hour_clk <= hour_clk + 1;
+								else hour_clk <= 0;
+								end if; --end hour_clk	
+							end if;--end min_clk						
+						end if;--end sec_clk
+					end if; --end count
+				
+				elsif(state = set_time) then
+					if(set_hour = '1') then
+						if(hour_set = 11) then
+							hour_set <= 0;
+						else 
+							hour_set <= hour_set + 1;
 						end if;
 					end if;
 				
-			end if;
-			
-			end if;
+					if(set_minutes='1') then
+						if(min_set < 59) then
+							min_set <= min_set + 1;
+						else min_set <= 0;
+						end if;
+					end if;
+			else
+				count <= count + 1;
+			end if;--end if clk
 		end if;
-end process;
-
+	end process;
 end Behavioral;
--- Test_CLK.vhd
+++ Test_CLK.vhd
@@ -2,10 +2,10 @@
 -- Company: 
 -- Engineer:
 --
--- Create Date:   16:52:43 02/14/2016
+-- Create Date:   00:04:31 04/07/2019
 -- Design Name:   
--- Module Name:   /home/almu/RelojBinario2/Test_CLK.vhd
--- Project Name:  RelojBinario2
+-- Module Name:   /home/almu/VHDL-Binary-Clock/Test_CLK.vhd
+-- Project Name:  Binary_Clock
 -- Target Device:  
 -- Tool versions:  
 -- Description:   
@@ -41,48 +41,56 @@ ARCHITECTURE behavior OF Test_CLK IS
  
     COMPONENT digi_clk
     PORT(
-         clk1 : IN  std_logic;
-         seconds : OUT  std_logic_vector(5 downto 0);
-         minutes : OUT  std_logic_vector(5 downto 0);
-         hours : OUT  std_logic_vector(3 downto 0);
+         clk : IN  std_logic;
+         seconds : INOUT  std_logic_vector(5 downto 0);
+         minutes : INOUT  std_logic_vector(5 downto 0);
+         hours : INOUT  std_logic_vector(3 downto 0);
          set_hour : IN  std_logic;
-         set_minutes : IN  std_logic
+         set_minutes : IN  std_logic;
+         set_t : IN  std_logic;
+         set_enable : OUT  std_logic
         );
     END COMPONENT;
     
 
    --Inputs
-   signal clk1 : std_logic := '0';
+   signal clk : std_logic := '0';
    signal set_hour : std_logic := '0';
    signal set_minutes : std_logic := '0';
+   signal set_t : std_logic := '0';
 
- 	--Outputs
+	--BiDirs
    signal seconds : std_logic_vector(5 downto 0);
    signal minutes : std_logic_vector(5 downto 0);
    signal hours : std_logic_vector(3 downto 0);
 
+ 	--Outputs
+   signal set_enable : std_logic;
+
    -- Clock period definitions
-   constant clk1_period : time := 31.25 ns;
+   constant clk_period : time := 10 ns;
  
 BEGIN
  
 	-- Instantiate the Unit Under Test (UUT)
    uut: digi_clk PORT MAP (
-          clk1 => clk1,
+          clk => clk,
           seconds => seconds,
           minutes => minutes,
           hours => hours,
           set_hour => set_hour,
-          set_minutes => set_minutes
+          set_minutes => set_minutes,
+          set_t => set_t,
+          set_enable => set_enable
         );
 
    -- Clock process definitions
-   clk1_process :process
+   clk_process :process
    begin
-		clk1 <= '0';
-		wait for clk1_period/2;
-		clk1 <= '1';
-		wait for clk1_period/2;
+		clk <= '0';
+		wait for clk_period/2;
+		clk <= '1';
+		wait for clk_period/2;
    end process;
  
 
@@ -90,11 +98,31 @@ BEGIN
    stim_proc: process
    begin		
       -- hold reset state for 100 ns.
-      wait for 100 ns;	
+      wait for 100 ns;
+		-- insert stimulus here 
 
-      wait for clk1_period*10;
-      -- insert stimulus here
+		for t in 0 to 160*4 loop
+			set_t <= '0';
+			--wait until clk'event and clk='1';
+			wait for 100 ns;
+		end loop;
 
+		set_t <= '1';
+		wait for 100 ns;
+		set_t <= '0';
+		wait for 100 ns;
+
+		for t in 0 to 160*4 loop
+			set_hour <= '1';
+			--wait until clk'event and clk='1';
+			wait for 100 ns;
+		end loop;
+		
+		set_hour <= '1';
+		--wait until clk'event and clk='1';
+      wait for 10 ns;
+		
+		wait for clk_period*10;
       wait;
    end process;
 
