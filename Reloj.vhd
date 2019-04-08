4debb131b74a769d0e377620584bd7c0b89c6022
AlmuHS <almuhs@github.com>
2019-04-09 00:14:06 +0200

    changed set_t to switch

--- Reloj.vhd
+++ Reloj.vhd
@@ -58,29 +58,25 @@ architecture Behavioral of digi_clk is
 	signal hour_set, hour_clk: integer range 0 to 12 := 0; 
 	signal count : integer := 1;
 	
-	constant clock: std_logic_vector(1 downto 0) := "00";
-	constant set_time: std_logic_vector(1 downto 0) := "01";
-	constant wait_to_clock: std_logic_vector(1 downto 0) := "10";
-	constant wait_to_set: std_logic_vector(1 downto 0) := "11";
+	constant clock: std_logic := '0';
+	constant set_time: std_logic := '1';
 	
-	signal state: std_logic_vector(1 downto 0) := clock;
+	signal state: std_logic := clock;
 	
 begin
 		
 	seconds <= conv_std_logic_vector(sec_clk,6) when(state = clock) else conv_std_logic_vector(0,6);
-	minutes <= conv_std_logic_vector(min_clk,6) when(state = clock and min_clk > min_set) else  conv_std_logic_vector(min_set, 6);
-	hours <= conv_std_logic_vector(hour_clk,4) when(state = clock and hour_clk > hour_set) else conv_std_logic_vector(hour_set,4);
+	minutes <= conv_std_logic_vector(min_clk,6) when(state = clock) else  conv_std_logic_vector(min_set, 6);
+	hours <= conv_std_logic_vector(hour_clk,4) when(state = clock) else conv_std_logic_vector(hour_set,4);
 
-	set_enable <= '0' when(state = clock or state = wait_to_set) else '1';
+	set_enable <= set_t;
 
 	transitions: process(set_t, clk)
 	begin
 		if(clk'event and clk='1') then
 			case state is
-				when clock => if(set_t = '1') then state <= wait_to_set; else state <= clock; end if; 
-				when wait_to_set => if(set_t = '0') then state <= set_time; else state <= wait_to_set; end if;
-				when set_time => if(set_t = '1') then state <= wait_to_clock; else state <= set_time; end if;
-				when wait_to_clock => if(set_t = '0') then state <= clock; else state <= wait_to_clock; end if;
+				when clock => if(set_t = '1') then state <= set_time; else state <= clock; end if; 
+				when set_time => if(set_t = '0') then state <= clock; else state <= set_time; end if;
 				when others => state <= clock;
 			end case;
 		end if;
@@ -91,46 +87,46 @@ begin
 	begin
 		if(clk'event and clk='1') then
 			if(count = 16000000) then
-				count <= 0;
+			count <= 0;
 
-				if(state = clock) then
-						min_clk <= conv_integer(minutes);
-						hour_clk <= conv_integer(hours);
-					
-						if(sec_clk < 59) then 
-							sec_clk <= sec_clk + 1;
-						else 
-							sec_clk <= 0;
-							
-							if(min_clk < 59) then min_clk <= min_clk + 1;
-							else 
-								min_clk <= 0;
-								if(hour_clk < 12) then 
-									hour_clk <= hour_clk + 1;
-								else hour_clk <= 0;
-								end if; --end hour_clk	
-							end if;--end min_clk						
-						end if;--end sec_clk
-					end if; --end count
+			if(state = clock) then
+					min_clk <= conv_integer(minutes);
+					hour_clk <= conv_integer(hours);
 				
-				elsif(state = set_time) then
-					if(set_hour = '1') then
-						if(hour_set = 11) then
-							hour_set <= 0;
+					if(sec_clk < 59) then 
+						sec_clk <= sec_clk + 1;
+					else 
+						sec_clk <= 0;
+						
+						if(min_clk < 59) then min_clk <= min_clk + 1;
 						else 
-							hour_set <= hour_set + 1;
-						end if;
+							min_clk <= 0;
+							if(hour_clk < 12) then 
+								hour_clk <= hour_clk + 1;
+							else hour_clk <= 0;
+							end if; --end hour_clk	
+						end if;--end min_clk						
+					end if;--end sec_clk
+				end if; --end count
+			
+			elsif(state = set_time) then
+				if(set_hour = '1') then
+					if(hour_set = 11) then
+						hour_set <= 0;
+					else 
+						hour_set <= hour_set + 1;
 					end if;
-				
-					if(set_minutes='1') then
-						if(min_set < 59) then
-							min_set <= min_set + 1;
-						else min_set <= 0;
-						end if;
+				end if;
+			
+				if(set_minutes='1') then
+					if(min_set < 59) then
+						min_set <= min_set + 1;
+					else min_set <= 0;
 					end if;
-			else
-				count <= count + 1;
-			end if;--end if clk
+				end if;
+		else
+			count <= count + 1;
+		end if;--end if clk
 		end if;
 	end process;
 end Behavioral;
